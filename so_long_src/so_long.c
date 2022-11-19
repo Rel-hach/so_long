a0b3ac0201cdc2cc0e0917f1c6ad705badb5e255
Rel-hach <rachidelhachimi6@gmail.com>
2022-11-19 01:48:05 +0100

    feat: initialize variables

--- so_long_src/so_long.c
+++ so_long_src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 01:19:55 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/11/16 21:01:47 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/11/19 01:45:39 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,20 @@ void	ft_check_extention(char *str)
 		ft_error_message("File format is not supported\n");
 }
 
+void	ft_variabes_initializer(t_game *g)
+{
+	g->player = 0;
+	g->p_x = 0;
+	g->p_y = 0;
+	g->space = 0;
+	g->collect = 0;
+	g->exit = 0;
+	g->wall = 0;
+	g->n_lines = 0;
+	g->size = 0;
+	g->move = 1;
+}
+
 int	main(int ac, char **av)
 {
 	t_game	g;
@@ -27,4 +41,6 @@ int	main(int ac, char **av)
 	if (ac != 2)
 		ft_error_message("Number of Arguments is invalid\n");
 	ft_check_extention(av[1]);
+	ft_variabes_initializer(&g);
+	read_map(av[1], &g);
 }
