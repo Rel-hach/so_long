105e5e09be2f2bbbe378f0fa3df0dfd3d022150e
Rel-hach <rachidelhachimi6@gmail.com>
2022-11-20 00:23:55 +0100

    add: create a window with mlx_new_window

--- so_long_src/so_long.c
+++ so_long_src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 01:19:55 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/11/20 00:12:27 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/11/20 00:22:03 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,17 @@ void	ft_check_extention(char *str)
 		str++;
 	if (ft_strcmp(".ber", str) != 0)
 		ft_error_message("File format is not supported\n");
+}
+
+void	ft_create_window(t_game *g)
+{
+	g->ptr_mlx = mlx_init();
+	if (g->ptr_mlx == NULL)
+		ft_error_message("coudn't create any connexion bet software and disp\n");
+	g->ptr_win = mlx_new_window(g->ptr_mlx, g->size * 50,
+			g->n_lines * 50, "so_long");
+	if (g->ptr_win == NULL)
+		ft_error_message("windows coundn't be created on screen\n");
 }
 
 void	ft_variabes_initializer(t_game *g)
@@ -44,4 +55,5 @@ int	main(int ac, char **av)
 	ft_variabes_initializer(&g);
 	read_map(av[1], &g);
 	ft_check_map(g);
+	ft_create_window(g);
 }
